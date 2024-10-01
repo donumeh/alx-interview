9080d4379aead2fbefcdb00da152442f47d5eb8f
Donald <donumeh54@gmail.com>
2024-10-02 00:15:31 +0100

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+
+"""
+Example solution:
+    [1]
+    [1,1]
+    [1,2,1]
+    [1,3,3,1]
+    [1,4,6,4,1]
+"""
+
+
+def pascal_triangle(n):
+    """
+    Design a pascal triangle using the int passed
+
+    Args:
+        n: int (the number of rows of the integer < n)
+    """
+
+    triangle = []  # triangle inits to list
+
+    for i in range(n):  # number of rows to print
+        row = []  # each row value will be added here
+
+        for j in range(i + 1):  # number of values in each row
+
+            if i == 0:
+                row.append(1)  # if first row, just append 1
+                break
+
+            prev_row = i - 1  # get the prev row
+            prev_first_col = j - 1  # get the prev col
+
+            value1 = (
+                0 if prev_first_col == -1 else triangle[prev_row][prev_first_col]
+            )  # index error checking
+            value2 = 0 if j > (i - 1) else triangle[prev_row][j]  # index error checking
+
+            sum_value = value1 + value2  # summing values
+            row.append(sum_value)
+
+        triangle.append(row)  # appending the rows in triangle
+
+    return triangle
+
+
+if __name__ == "__main__":
+    print(pascal_triangle(5))
