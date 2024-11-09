949322cd4df570a92df6804d180e32fb81cf639e
Donald <donumeh54@gmail.com>
2024-11-09 03:18:00 +0100

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -29,9 +29,7 @@ def parse_line(line):
     updates status_counts based on the status code and file size
     """
 
-    pattern = (
-        r'(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)'
-    )
+    pattern = r'((\d*\.?){1,3}){4} - \[(\d*\-?){3} (\d*:?){3}\.\d*\] "GET /projects/260 HTTP/1\.1" \d{3} \d{1,4}'
     match = re.match(pattern, line)
 
     if match:
