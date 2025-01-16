5546e01b3d450ed55e4daa7bb085eef3bc08866b
Donald <donumeh54@gmail.com>
2025-01-16 22:15:40 +0100

    prime-game correction

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,41 @@
 #!/usr/bin/python3
 """ Module for solving prime game question """
 
+
 def isWinner(x, nums):
-    """function that checks for the winner"""
-    if not nums or x < 1:
+    """is Winner function"""
+    if x < 1 or not nums:
         return None
+
     max_num = max(nums)
 
-    my_filter = [True for _ in range(max(max_num + 1, 2))]
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not my_filter[i]:
-            continue
-        for j in range(i * i, max_num + 1, i):
-            my_filter[j] = False
-    my_filter[0] = my_filter[1] = False
-    y = 0
-    for i in range(len(my_filter)):
-        if my_filter[i]:
-            y += 1
-        my_filter[i] = y
-    player1 = 0
-    for x in nums:
-        player1 += my_filter[x] % 2 == 1
-    if player1 * 2 == len(nums):
-        return None
-    if player1 * 2 > len(nums):
+    # Step 1: Compute primes up to max_num using Sieve of Eratosthenes
+    primes = [True] * (max_num + 1)
+    primes[0] = primes[1] = False  # 0 and 1 are not prime
+    for i in range(2, int(max_num**0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, max_num + 1, i):
+                primes[j] = False
+
+    # Step 2: Precompute the number of primes up to each number
+    prime_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
+
+    # Step 3: Determine the winner for each round
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    # Step 4: Return the overall winner
+    if maria_wins > ben_wins:
         return "Maria"
-    return "Ben"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
